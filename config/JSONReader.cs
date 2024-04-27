adb567f835070ef10f2df792c4368965df91dd06
VigilBushido <vigilbushido@gmail.com>
2024-04-27 17:28:54 -0400

    Initial Commit - Online

---
+++ config/JSONReader.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GroguBot.config
+{
+    public class JSONReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config/config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
+
+                this.token = data.token;
+                this.prefix = data.prefix;
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
