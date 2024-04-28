2e7e8b20770904acef3dfd67210e56046cb62121
VigilBushido <vigilbushido@gmail.com>
2024-04-27 21:42:36 -0400

    + cmd: add 2 hexadecimals

--- commands/TestCommands.cs
+++ commands/TestCommands.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -12,7 +13,41 @@ namespace GroguBot.commands
         [Command("test")]
         public async Task BasicCommand(CommandContext ctx)
         {
-            await ctx.Channel.SendMessageAsync("Grogu Instantiated");
+            await ctx.Channel.SendMessageAsync($"Grogu Instantiated.. I am at your command {ctx.User.Username}");
+        }
+
+
+        [Command("addhex")]
+        public async Task AddHex(CommandContext ctx, string hex_one, string hex_two)
+        {
+            BigInteger result = HexLiteralToLong(hex_one) + HexLiteralToLong(hex_two);
+            await ctx.Channel.SendMessageAsync(result.ToString());
+        }
+
+        private static long HexLiteralToLong(string hex)
+        {
+            if (string.IsNullOrEmpty(hex)) throw new ArgumentException("hex");
+
+            int i = hex.Length > 1 && hex[0] == '0' && (hex[1] == 'x' || hex[1] == 'X') ? 2 : 0;
+            long value = 0;
+
+            while (i < hex.Length)
+            {
+                int x = hex[i++];
+
+                if
+                    (x >= '0' && x <= '9') x = x - '0';
+                else if
+                    (x >= 'A' && x <= 'F') x = (x - 'A') + 10;
+                else if
+                    (x >= 'a' && x <= 'f') x = (x - 'a') + 10;
+                else
+                    throw new ArgumentOutOfRangeException("hex");
+
+                value = 16 * value + x;
+            }
+
+            return value;
         }
     }
 }
